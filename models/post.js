aa60f051ee5e5eb874cb1cd85ca7b5737652d93a
eles linux <eleswastaken@gmail.com>
2022-03-08 08:08:32 +0600

    alter post model

--- models/post.js
+++ models/post.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
-	title: {type: String},
-	body: {type: String},
-	date: {type: Date},
+	title: {type: String, required: true},
+	body: {type: String, required: true},
+	date: {type: Date, required: true},
 });
 
 
