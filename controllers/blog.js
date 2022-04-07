d79416f8590edec5b51449d522e2fb8cf673fd16
eles linux <eleswastaken@gmail.com>
2022-04-07 17:42:33 +0600

    integrate markdown renderer

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,4 +1,5 @@
 
+const showdown = require('showdown');
 const Post = require('../models/post');
 
 exports.getIndex = function(req, res) {
@@ -15,10 +16,15 @@ exports.getPost = async function(req, res) {
 
 	const post = await Post.findById(req.params.id);
 
+	converter = new showdown.Converter();
+
 	if (!post) {
 		res.sendStatus(404)
 		return 
 	}
+
+	htmlBody = converter.makeHtml(post.body)
+	post.body = htmlBody
 
 	res.render('blogPost', { post });
 
